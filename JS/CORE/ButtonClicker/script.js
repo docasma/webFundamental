30a9a45d0f0644dfa23ef1e0f1332e32bdf084ef
docasma <asmabenhasine@yahoo.fr>
2023-10-12 23:01:51 +0100

    first commit

---
+++ JS/CORE/ButtonClicker/script.js
@@ -0,0 +1,17 @@
+console.log("page loaded")
+
+function message(){
+    alert("Ninja was liked!")
+}
+
+function login(element){
+    if(element.innerText=="login"){
+        element.innerText="logout";
+    }else{
+        element.innerText="login"
+    }
+}
+
+function hide(element){
+   element.remove()
+}
